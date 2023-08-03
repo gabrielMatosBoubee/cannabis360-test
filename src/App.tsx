f93c753880deaa6c1c723397e11ca7362188eda6
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-08-03 00:34:59 -0300

    feat: add react query and react datapicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-// import './App.css'
+import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 import Requests from './pages/Requests'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 function App() {
   const router = createBrowserRouter([{
@@ -9,8 +10,12 @@ function App() {
     element: <Layout><Requests /></Layout>
   }])
 
+  const queryClient = new QueryClient();
+
   return (
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+  </QueryClientProvider>
   )
 }
 
